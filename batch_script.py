6e00d99c2bc99ab0ff774fa41b8627881cf5f4c0
Andrew D. Yates <andrewyates.name@gmail.com>
2012-10-05 00:51:39 -0400

    batch_script.py file names and docs

--- batch_script.py
+++ batch_script.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 """Manual script for computing all-pairs dependency matrix batches.
 
+
+
 Saves files in outdir in pattern:
-  TODO
+
+    [outdir]/[batchname].[computer.MNAME].npy
+    
+    where [batchname] matches
+      self: RX_SELF_BATCHNAME
+        [matrix_fname]_[start]_[end]_self
+      dual: RX_DUAL_BATCHNAME
+        [matrix_fname_1]_[matrix_fname_2]_[offset]_dual
+
+    EXAMPLE SAVED FILE:
+      /path/outdir/GSE15745-GPL6104.data.aligned.pkl_GSE15745-GPL8490.data.aligned.pkl_0_dual.STD_PRODUCT.npy
 
 EXAMPLE USE:
 time python /nfs/01/osu6683/recomb2013_workbench/dependency_matrix/batch_script.py computer=PCC fname2=/fs/lustre/osu6683/recomb2013_gse15745/dispatch_test/GSE15745_GPL8490.data.aligned.pkl fname1=/fs/lustre/osu6683/recomb2013_gse15745/dispatch_test/GSE15745_GPL6104.data.aligned.pkl offset=0 verbose=True
@@ -45,18 +57,22 @@ def main(computer=None, compute_options=None, outdir=None, fname=None, start=Non
       end = n*(n-1)/2
     else:
       end = int(end)
-    batchname = "%s_%d_%d" % (fclean(fname), start, end)
+    batchname = "%s_%d_%d_self" % (fclean(fname), start, end)
     C = compute_self(M, computer, start, end, compute_options, verbose)
   # Dual matrices
   else:
     M1, M2 = load(fname1)["M"], load(fname2)["M"]
     if offset is not None:
       offset = int(offset)
-      batchname = "%s_%s_%d" % (fclean(fname1), fclean(fname2), offset)
+      batchname = "%s_%s_%d_dual" % (fclean(fname1), fclean(fname2), offset)
     else:
-      batchname = "%s_%s_all"
+      batchname = "%s_%s_all_dual"
     C = compute_dual(M1, M2, computer, offset, compute_options, verbose)
 
+  if verbose:
+    print batchname
+    print "match rx self?", RX_SELF_BATCHNAME.match(batchname)
+    print "match rx dual?", RX_DUAL_BATCHNAME.match(batchname)
   assert RX_SELF_BATCHNAME.match(batchname) != RX_DUAL_BATCHNAME.match(batchname)
   n_nans = C.nans()
   if n_nans > 0:
