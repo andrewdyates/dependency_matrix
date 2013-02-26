4efb8d420d9b67a7adfc97acd2f5321ef830c8d9
Andrew D. Yates <andrewyates.name@gmail.com>
2013-02-25 20:57:23 -0500

    fixed minor bugs in parameter types

--- permutation_test_dispatch_script.py
+++ permutation_test_dispatch_script.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 """Permute rows of matrix before dispatching. On computation completion, generate report.
+
+Shells Calls similar to dispatch_script.py
+
+EXAMPLE:
+  $ python $HOME/pymod/dependency_matrix/permutation_test_dispatch_script.py n_permutes=2 fname=$HOME/gibbs_feb16_cleaned_data/gibbs.mrna.all.psb.corr.feb-15-2013.tab computers=[\"PCC\"] outdir=/fs/lustre/osu6683/gse15745_feb/all_mrna_perm_test n_nodes=10 n_ppn=12 hours=6
 """
 import random
 import numpy as np
@@ -8,6 +13,7 @@ from qsub import *
 from matrix_io import *
 import datetime
 import dispatch_script
+import sys
 
 def permute_rows(M, seed=True):
   """Randomly permute values in rows of M. Preserve masks. Permutes in place."""
@@ -29,18 +35,13 @@ def permute_rows(M, seed=True):
       M[i,:].mask = np.isnan(row)
 
 # NOTE: n_permutes is number of matrix permutes. Total permutations = #rows_1*#row_2*n_permutes
-def main(n_permutes=1, fname=None, fname1=None, fname2=None, computers=None, outdir=None, n_nodes=1, n_ppn=12, hours=8, compute_options=None, dry=False):
+def main(n_permutes=1, fname=None, fname1=None, fname2=None, outdir=None, dry=False, **kwds):
+  print outdir
   n_permutes = int(n_permutes)
   assert n_permutes >= 1
   if not os.path.exists(outdir):
     make_dir(outdir)
     print "Created outdir %s" % outdir
-  n_ppn = int(n_ppn)
-  n_nodes = int(n_nodes)
-  hours = int(hours)
-  assert n_nodes >= 1
-  assert n_ppn >= 1 and n_ppn <= 12
-  assert hours >= 1 and hours <= 99
   os.chdir(outdir)
 
   now_timestamp = datetime.datetime.now().isoformat('_')
@@ -70,22 +71,22 @@ def main(n_permutes=1, fname=None, fname1=None, fname2=None, computers=None, out
     ## Dispatch dependency computation
     if mtype == "self":
       print "Permuting %s, iteration %d..." % (fname, i)
-      M_p = permute_rows(M)
+      permute_rows(M)
       perm_fname = bname(fname).rpartition(".")[0] + ".perm%d.pkl"%i
       perm_path = os.path.join(perm_dir, perm_fname)
       print "Saving permutated copy as %s..." % perm_path
-      save(M_p, perm_path)
-      print "Calling dispatcher..."
-      R = dispatch_script.main(fname=perm_path, computers=computers, outdir=perm_dir, n_nodes=n_nodes, n_ppn=n_ppn, hours=hours, compute_options=compute_options, dry=dry)
+      print save(M, perm_path, ftype="pkl")
+      print "Calling dispatcher......\n................"
+      R = dispatch_script.main(fname=perm_path, outdir=perm_dir, dry=dry, **kwds)
     else:     # mtype == "dual"
       print "Permuting %s, iteration %d..." % (fname1, i)
-      M_p = permute_rows(M1)
+      permute_rows(M1)
       perm_fname = bname(fname1).rpartition(".")[0] + ".perm%d.pkl"%i
       perm_path = os.path.join(perm_dir, perm_fname)
       print "Saving permutated copy as %s..." % perm_path
-      save(M_p, perm_path)
+      save(M1, perm_path, ftype="pkl")
       print "Calling dispatcher..."
-      R = dispatch_script.main(fname1=perm_path, fname2=fname_work2, computers=computers, outdir=perm_dir, n_nodes=n_nodes, n_ppn=n_ppn, hours=hours, compute_options=compute_options, dry=dry)
+      R = dispatch_script.main(fname1=perm_path, fname2=fname_work2, outdir=perm_dir, dry=dry, **kwds)
 
     PIDs.append(R['pid'])
     JSONs.append(R['exelog_fname_json'])
@@ -101,9 +102,12 @@ def main(n_permutes=1, fname=None, fname1=None, fname2=None, computers=None, out
   cmd = shell_permutation_compile(json_fnames=JSONs, out_fname=out_fname)
   Q.add(cmd)
   pid = Q.submit(dry)
+  print Q.script()
   print "Final permutation PID: %s" % pid
   return pid
 
       
 if __name__ == "__main__":
-  main()
+  argv = dict([s.split('=') for s in sys.argv[1:]])
+  print argv
+  main(**argv)
