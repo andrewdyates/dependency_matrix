2d229f4289b00321e7c3c3be99ee695a978af547
Andrew D. Yates <andrewyates.name@gmail.com>
2013-02-27 18:29:09 -0500

    fixed permutation compilation reports

--- permutation_test_compile_script.py
+++ permutation_test_compile_script.py
@@ -19,13 +19,24 @@ def main(jsons="", out_fname=None):
       M = load(md['values'])['M']
       print >> out, "np.size(M)", np.size(M)
       print >> out, "M.shape", M.shape
-      mask = np.isnan(M)
-      print >> out, "np.sum(np.isnan(M))", np.sum(mask)
+      nan_mask = np.isnan(M)
+      print >> out, "np.sum(np.isnan(M))", np.sum(nan_mask)
       print >> out, "masking nans..."
       # There's a bug in np.sum of float32s;
       #   this distorts the mean (but not histogram)
       #   convert to float64 to correct this issue
-      print >> out, "Removed %d masked values from consideration; converted matrix to float64 to mitigate np.sum float32 bug." % (np.sum(mask))
+      print >> out, "Removed %d masked values from consideration." % (np.sum(nan_mask))
+      print >> out, "Converted matrix to float64 to mitigate np.sum float32 bug."
+      # Remove missing values from consideration
+      if 'missing' in md:
+        print >> out, "Missing values reported to exist. Loading IsSet matrix %s." % md['missing']
+        B = load(md['missing'])['M']
+        assert B.shape == M.shape and B.size == M.size
+        n_missing = np.sum(B==0)
+        print >> out, "%d missing values, %d existing values, %d total values." % (n_missing, B.size-n_missing, B.size)
+        isset_mask = np.array(B)
+        mask = nan_mask|(~isset_mask)
+        print >> out, "%d total masked values (%d nans, %d missing)" % (np.sum(mask), np.sum(nan_mask), np.sum(~isset_mask))
       Q=np.array(M[~mask], dtype=np.float)
       print >> out, "np.sum(Q)", np.sum(Q)
       print >> out, "np.size(Q)", np.size(Q)
@@ -36,8 +47,8 @@ def main(jsons="", out_fname=None):
       print >> out, "np.median(Q)", np.median(Q)
       h = np.histogram(Q)
       print >> out, "Histogram:"
-      print >> out, "bins: ", ",".join(map(str, h[0]))
-      print >> out, "counts: ", ",".join(map(str, h[1]))
+      print >> out, "counts: ", ",".join(map(str, h[0]))
+      print >> out, "bins: ", ",".join(map(str, h[1]))
       print >> out, "==="
 
 if __name__ == "__main__":
