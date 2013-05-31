2aaa862f1689b06bf22800f8382eca75e20aaa5e
Andrew D. Yates <andrewyates.name@gmail.com>
2013-05-31 00:22:12 -0400

    compile to R wrapper included

--- compile_to_R_dispatch_wrapper.py
+++ compile_to_R_dispatch_wrapper.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 """Wrapper to compile .pkl dep matrix result to .RData
+
+python $HOME/pymod/dependency_matrix/compile_to_R_dispatch_wrapper.py fname=$HOME/brca/GSE31448.SCAN.tab computers=[\"PCC\"] outdir=$HOME/test n_nodes=10 n_ppn=12 hours=10
 """
 import dispatch_script
 import qsub
+import os, sys
 WRAPPER_FNAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "compile_to_R_from_json.py")
 
 def main(**kwds):
@@ -21,7 +24,7 @@ def main(**kwds):
     raise Exception, "No matrix filenames fname or (fname1, fname2)"
     
   cmd = "time python %(script)s fname=%(fname)s row_fname=%(row_fname)s col_fname=%(col_fname)s outdir=%(outdir)s" % {'script':WRAPPER_FNAME, 'fname':fname, 'row_fname':row_fname, 'col_fname':col_fname, 'outdir':kwds['outdir']}
-  Q = qsub.Qsub(hours=12, ppn=8)
+  Q = qsub.Qsub(hours=12, n_ppn=8, jobname="toR_%s"%pid)
   Q.add(cmd)
   pid = Q.submit(dry)
   print Q.script()
